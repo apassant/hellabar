ca3e062c72aae284c50f8a057740b190eeedad0f
Alexandre Passant <alex@seevl.net>
2013-09-30 14:24:14 -0700

    JSON compilation file for multiple cities

---
+++ json/compile.py
@@ -0,0 +1,42 @@
+import json
+
+data_folder = 'output'
+## Get this from a config file
+# f = open("%s/config.json" %(data_folder), 'r')
+# cities = json.loads(f.read())
+# f.close()
+
+cities = {
+    'ny' : {
+        'name' : 'New York',
+        'coordinates' : {
+            'lat' : 0,
+            'long' : 0
+        }
+    }, 
+    'oakland' : {
+        'name' : 'Oakland',
+        'coordinates' : {
+            'lat' : 10,
+            'long' : 10
+        }
+    }, 
+    'sf' : {
+        'name' : 'San Francisco',
+        'coordinates' : {
+            'lat' : 30,
+            'long' : 30
+        }
+    },
+}
+
+for data in ['genres', 'venues']:
+    for city in cities.keys():
+        f = open("%s/%s/%s.json" %(data_folder, city, data), 'r')
+        cities[city].update({
+            'data' : json.loads(f.read())
+        })
+        f.close()
+    f = file("%s.json" %(data), 'w')
+    f.write(json.dumps(cities))
+    f.close()
